078cd9a8f198858e3d5940986cb795fe0db22868
Kirk Hayes <kph@Kirks-MacBook-Pro.local>
2016-01-24 16:35:48 -0500

    Fix issues with path data parsing.

--- shareParser.py
+++ shareParser.py
@@ -11,9 +11,11 @@ import nmap
 #	   - SSNs
 #	   - Credit Card Numbers
 #	   - More?
+root = 'mnt'
+
 def nmapPortScan():
 	port='111'
-	hosts='192.168.108.0/24'
+	hosts='172.16.133.134'
 	IP = []
 	print('[+] Scanning ' + hosts + ' for port ' + port)
 	nm=nmap.PortScanner()
@@ -37,26 +39,26 @@ def nfsmount(ip):
 		results=anonNFS.split(' ')[0]
 		results=str(results)
 		print('[+] Mounting NFS share: ' + results)
-		subprocess.check_output('mount -t nfs '+ ip + ':' + results + ' mnt/', shell=True)
-		print('[+] NFS share mounted to ./mnt')
-		subprocess.check_output('cd mnt/', shell=True)
-		fileSearch(results)
+		subprocess.check_output('mount -t nfs '+ ip + ':' + results + ' ' + root, shell=True)
+		print('[+] NFS share mounted to ' + root)
+		subprocess.check_output('cd ' + root, shell=True)
+		fileSearch(root)
 		# unmount share
 		print('[+] Unmounting mount: ' + results)
-		subprocess.check_output('umount mnt/', shell=True)
+		subprocess.check_output('umount ' + root, shell=True)
 	else:
 		print('[-] No Anonymous NFS shares found')
 
 
-def fileSearch(results):
-	root = 'mnt/'
-	print('[+] Searching for files in mount...')
+def fileSearch(root):
+	print('[+] Searching for files in: ' + root)
 	for path, subdirs, files in os.walk(root):
 		for name in files:
-			ssnSearch(results, name)
-			CCSearch(results, name)
-		passSearch(files, path)
-
+			ssnSearch(path, name)
+			CCSearch(path, name)
+		#passSearch(files, path)
+		for subdir in subdirs:
+			fileSearch(subdir)
 
 def passSearch(files, path):
 	print('[+] Looking for password files')
@@ -64,9 +66,9 @@ def passSearch(files, path):
 		if 'password' in file.lower():
 			print('[!] Potential Password File: ' + path + file)
 
-def CCSearch(results, name):
-	print('[+] Looking for Credit Card Numbers in: ' + name)
-	with open('mnt/' + name, 'r') as f:
+def CCSearch(path, name):
+	print('[+] Looking for Credit Card Numbers in: ' + path + '/' + name)
+	with open(path + '/' + name, 'r') as f:
 		for line, number in enumerate(f, 1):
 			prog = re.compile('(\D|^)\%?[Bb]\d{13,19}\^[\-\/\.\w\s]{2,26}\^[0-9][0-9][01][0-9][0-9]{3}|(\D|^)\;\d{13,19}\=(\d{3}|)(\d{4}|\=)|[1-9][0-9]{2}\-[0-9]{2}\-[0-9]{4}\^\d|(\D|^)5[1-5][0-9]{2}(\ |\-|)[0-9]{4}(\ |\-|)[0-9]{4}(\ |\-|)[0-9]{4}(\D|$)|(\D|^)4[0-9]{3}(\ |\-|)[0-9]{4}(\ |\-|)[0-9]{4}(\ |\-|)[0-9]{4}(\D|$)|(\D|^)(34|37)[0-9]{2}(\ |\-|)[0-9]{6}(\ |\-|)[0-9]{5}(\D|$)|(\D|^)30[0-5][0-9](\ |\-|)[0-9]{6}(\ |\-|)[0-9]{4}(\D|$)|(\D|^)(36|38)[0-9]{2}(\ |\-|)[0-9]{6}(\ |\-|)[0-9]{4}(\D|$)|(\D|^)6011(\ |\-|)[0-9]{4}(\ |\-|)[0-9]{4}(\ |\-|)[0-9]{4}(\D|$)')
 			result = prog.search(number)
@@ -74,9 +76,9 @@ def CCSearch(results, name):
 				print('[!] MATCH: Credit Card Data found in line number: ' + str(line))
 		f.close()
 
-def ssnSearch(results, name):
-	print('[+] Looking for SSNs in: ' + name)
-	with open('mnt/' + name, 'r') as f:
+def ssnSearch(path, name):
+	print('[+] Looking for SSNs in: ' + path + '/' + name)
+	with open(path + '/' + name, 'r') as f:
 		for line, number in enumerate(f, 1):
 			# Look for SSNs with - and with spaces
 			prog = re.compile('(\D|^)[0-9]{3}\-[0-9]{2}\-[0-9]{4}(\D|$)|(\D|^)[0-9]{3}\ [0-9]{2}\ [0-9]{4}(\D|$)')
